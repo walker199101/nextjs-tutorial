f9e3f0f185c8d6c105ef996ae71faf07753cdb6a
g-kim <walker199101@gmail.com>
2024-09-12 12:08:13 +0900

    Initial commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import variables from './../variables.module.scss'
+
+export default function Dashboard() {
+    return (
+      <div>
+        <p style={{ color: variables.primaryColor }}>This is the Dashboard page.</p>
+      </div>
+    );
+}
